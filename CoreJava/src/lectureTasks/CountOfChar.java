aee7761e4efeac04ddc5cddfa7735340e561647f
mustafacetinkaya <macetinkaya.03@gmail.com>
2022-10-18 10:15:12 +0300

    New tasks are added

---
+++ CoreJava/src/lectureTasks/CountOfChar.java
@@ -0,0 +1,22 @@
+package lectureTasks;
+
+public class CountOfChar {
+    public static void main(String[] args) {
+        String sentence="asdkljasdjhadfkjhafkjhasf";
+        int count=0;
+        String result="";
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch=sentence.charAt(i);
+
+            for (int j = 0; j < sentence.length(); j++) {
+                char each=sentence.charAt(j);
+
+                if(each==ch)
+                    count++;
+            }result=result+ch;
+            result+=count+" / ";
+        }
+        System.out.println(result);
+    }
+}
