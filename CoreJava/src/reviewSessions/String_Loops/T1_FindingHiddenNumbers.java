bdbeb7fdce75f74ee963ed7041b863ebb83a0224
mustafacetinkaya <macetinkaya.03@gmail.com>
2022-10-24 23:59:48 +0300

    New tasks are added

---
+++ CoreJava/src/reviewSessions/String_Loops/T1_FindingHiddenNumbers.java
@@ -0,0 +1,37 @@
+package reviewSessions.String_Loops;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class T1_FindingHiddenNumbers {
+/*
+
+ -- Task 1: Find the number taht given a hidden number between 1 and 100 randomly created.
+
+ Hint:   Random random=new Random();
+
+        int hiddenNumber = random.nextInt(100) + 1;
+ */
+
+    public static void main(String[] args) {
+
+        // int hidenNumber=6; this is not logical
+        Random random=new Random();
+        // create a number from 0 (inclusive) to 10 (exclusive)
+        int hiddenNumber = random.nextInt(10) + 1; // we have one number in that variable from 1 to 10
+        Scanner scanner=new Scanner(System.in);
+        boolean flag=true;
+        do {
+            System.out.println("Enter a number");
+            int searchedNumber= scanner.nextInt();
+            if (searchedNumber==hiddenNumber){
+                System.out.println("you find number Congrats!");
+                flag=false;
+            }else {
+                System.out.println("try again!");
+            }
+
+        }while (flag);
+
+    }
+}
---
+++ CoreJava/src/reviewSessions/String_Loops/T3_CountEachWord.java
@@ -0,0 +1,57 @@
+package reviewSessions.String_Loops;
+
+public class T3_CountEachWord {
+
+    /*
+    Interview Question:
+
+    --  Task 3: Count each name in sentence.
+
+    input :
+
+  String names = "Adam Adam Barry Aysun Aysun";
+
+    output:
+
+    Adam-2 Barry-1 Aysun-2
+     */
+
+    public static void main(String[] args) {
+
+        String names = "Adam Adam Barry Aysun Aysun";
+
+        String temp="";
+
+        String result="";
+        for (int i = 0; i < names.length(); i++) {
+            char ch=names.charAt(i);
+
+            //we need to find the word one by one
+            if (ch!=' '){
+                temp+=ch;
+                continue; // we need to find word in order to continue to next statement
+            }
+
+            String search = temp;
+
+            //in order to count word
+            int counter = 0;
+
+            String dumy=names;
+
+            while (dumy.contains(search)) {
+                counter++;
+                dumy = dumy.replaceFirst(search, "");
+            }
+            if (!result.contains(search))
+                result+=search + "-"+ counter +" ";
+
+            temp="";
+
+
+        }
+
+
+        System.out.println("result = " + result);
+    }
+}
