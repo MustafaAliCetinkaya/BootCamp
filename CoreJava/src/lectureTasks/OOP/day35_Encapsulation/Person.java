7f8dbbf8f055845490b5427e1d99f4d2d3bdf445
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-11-29 15:27:53 +0300

    New files have been uploaded.

---
+++ CoreJava/src/lectureTasks/OOP/day35_Encapsulation/Person.java
@@ -0,0 +1,31 @@
+package lectureTasks.OOP.day35_Encapsulation;
+public class Person {
+
+    private String name;
+    private int age;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        if(age <= 0 || age > 150){
+            System.err.println("Invalid Age: "+age);
+            System.exit(0);
+        }
+
+        this.age = age;
+    }
+
+
+
+}
