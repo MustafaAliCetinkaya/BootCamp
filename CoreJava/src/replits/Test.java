46bef071e4aeee6b32b16d48c0c375c60aea3374
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-05 00:40:50 +0300

    New files have been uploaded.

---
+++ CoreJava/src/replits/Test.java
@@ -0,0 +1,17 @@
+package replits;
+public class Test{
+    public Test () {
+        System.out.print ("A ");
+    }
+    public Test(int a) {
+        this ();
+        System.out.print ("B ");
+    }
+    public Test(String str){
+        this (100);
+        System.out.print(str);
+    }
+    public static void main (String[] args) {
+        Test obj = new Test(7);
+    }
+}
