59ca311d5687bdbcc7ba927eab3e339550873e64
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-22 12:33:16 +0300

    New assigment questions have been solved

---
+++ CoreJava/src/officeHour/recepBySolvingProblems/Test.java
@@ -0,0 +1,11 @@
+package officeHour.recepBySolvingProblems;
+
+public class Test {
+    public static void main(String[] args) {
+        final int number;
+        number=10;
+        //number=30;//As we expect, throws error
+
+        final int number1=20;//Different variable name (different signature)
+    }
+}
