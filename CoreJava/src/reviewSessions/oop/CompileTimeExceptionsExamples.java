97d8266a02044186ec6810620967fdf8567033fd
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-24 23:09:18 +0300

    New assigment questions have been solved

---
+++ CoreJava/src/reviewSessions/oop/CompileTimeExceptionsExamples.java
@@ -0,0 +1,20 @@
+package reviewSessions.oop;
+
+public class CompileTimeExceptionsExamples {
+
+    public static void main(String[] args) {
+        System.out.println("Adam");
+        waitFor(3);
+        System.out.println("Ersin");
+        //    Thread.sleep(3000);
+
+    }
+
+    private static void waitFor(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
