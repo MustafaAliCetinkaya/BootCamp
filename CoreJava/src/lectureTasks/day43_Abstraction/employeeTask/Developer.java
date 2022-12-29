cc168cca937d6068a179d379e448d0468e7d4804
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-29 22:42:45 +0300

    Abstraction/Inheritance/Encapsulation projects have been uploaded

---
+++ CoreJava/src/lectureTasks/day43_Abstraction/employeeTask/Developer.java
@@ -0,0 +1,28 @@
+package lectureTasks.day43_Abstraction.employeeTask;
+
+
+public final class Developer extends Employee{
+
+
+    public Developer(String name, int age, char gender, int id, String jobTitle, double salary) {
+        super(name, age, gender, id, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getName()+" is developing application");
+    }
+
+
+    @Override
+    public void sleep() {
+        System.out.println(getName()+" sleeps 6 hours");
+    }
+
+    public void unitTest(){
+        System.out.println(getName()+" is unit testing");
+    }
+
+
+
+}
