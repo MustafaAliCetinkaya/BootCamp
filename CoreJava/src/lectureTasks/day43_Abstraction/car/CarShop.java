d7baef6f74c3bc5773c51018c0edb6e60cad4e65
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2022-12-31 22:47:33 +0300

    Abstraction/Inheritance/Encapsulation projects have been uploaded

--- CoreJava/src/lectureTasks/day43_Abstraction/car/CarShop.java
+++ CoreJava/src/lectureTasks/day43_Abstraction/car/CarShop.java
@@ -5,7 +5,7 @@ public class CarShop {
     public static void main(String[] args) {
 
         //    Car car1 = new Car("Cydeo", "25", "Blue", 2022, 100000);
-        // we can not create object from Abstract class, because abstract class is not concrete
+        // We "can not create object" from Abstract class, because abstract class is "not concrete"
 
 
         Honda honda = new Honda("Accord", "Black", 2019, 30000);
