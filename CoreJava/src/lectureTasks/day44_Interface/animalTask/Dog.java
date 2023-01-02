e4a976f04586d93c6a6aeb78297b05b7d8755ab7
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-02 21:37:10 +0300

    Abstraction/Inheritance/Encapsulation projects have been uploaded

--- CoreJava/src/lectureTasks/day44_Interface/animalTask/Dog.java
+++ CoreJava/src/lectureTasks/day44_Interface/animalTask/Dog.java
@@ -1,23 +1,22 @@
 package lectureTasks.day44_Interface.animalTask;
 
-public final class Dog extends Animal implements Playable{
+public final class Dog extends Animal implements Playable {
 
     public Dog(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
     }
 
-    public void bark(){
-        System.out.println(getName()+" is barking");
+    public void bark() {
+        System.out.println(getName() + " is barking");
     }
 
-
+    @Override
     public void eat() {
-        System.out.println(getName()+" eats Pizza");
+        System.out.println(getName() + " eats Pizza");
     }
 
-    public void play(){
-        System.out.println(getName() +" is playing with ball");
+    @Override
+    public void play() {
+        System.out.println(getName() + " is playing with ball");
     }
-
-
 }
