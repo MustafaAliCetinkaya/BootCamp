6f0703cb93f22707c55186e4d5622eadc938583c
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-06 22:38:42 +0300

    Abstraction/Inheritance/Interface projects have been uploaded.

--- CoreJava/src/lectureTasks/day44_45_Interface/shapeTask/Volume.java
+++ CoreJava/src/lectureTasks/day44_45_Interface/shapeTask/Volume.java
@@ -5,7 +5,7 @@ public interface Volume {
     boolean hasVolume= true;
 
      double volume();
-
+public static final int number=10; //By default, it is already public, static, final.
    /*
     default void drink(){
          System.out.println("Drinks water");
