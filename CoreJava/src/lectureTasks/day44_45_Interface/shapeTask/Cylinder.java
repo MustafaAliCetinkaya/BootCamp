6fa5b290559cc421766fe341e895961fe1cd257a
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-07 00:28:58 +0300

    Abstraction/Inheritance/Interface projects have been uploaded.

--- CoreJava/src/lectureTasks/day44_45_Interface/shapeTask/Cylinder.java
+++ CoreJava/src/lectureTasks/day44_45_Interface/shapeTask/Cylinder.java
@@ -1,16 +1,44 @@
 package lectureTasks.day44_45_Interface.shapeTask;
 
+import static lectureTasks.day44_45_Interface.shapeTask.Circle.pi;
+
 public class Cylinder extends Shape implements Volume{
 
+    private double radius;
+    private double height;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius < 0){
+            throw new RuntimeException("Invalid Radius: "+radius);
+        }
+        this.radius = radius;
+    }
+
+    public double height() {
+        return radius;
+    }
+
+    public void height(double height) {
+        if(radius < 0){
+            throw new RuntimeException("Invalid Radius: "+height);
+        }
+        this.height = height;
+    }
 
 
-    public Cylinder(String name) {
-        super(name);
+    public Cylinder(double radius, double height) {
+        super("Cylinder");
+        this.radius = radius;
+        this.height = height;
     }
 
     @Override
     public double area() {
-        return 0;
+        return 2 * pi * radius * (radius + height);
     }
 
     @Override
@@ -21,6 +49,6 @@ public class Cylinder extends Shape implements Volume{
 
     @Override
     public double volume() {
-        return 0;
+        return radius * radius * pi * height;
     }
 }
