4db9f39aeac135ffa209799c0006b127730a0651
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-07 15:03:11 +0300

    Abstraction/Inheritance/Interface projects have been uploaded.

--- CoreJava/src/lectureTasks/day44_45_Interface/car/CarShop.java
+++ CoreJava/src/lectureTasks/day44_45_Interface/car/CarShop.java
@@ -5,18 +5,21 @@ public class CarShop {
     public static void main(String[] args) {
 
         //    Car car1 = new Car("Cydeo", "25", "Blue", 2022, 100000);
-        // We "can not create object" from Abstract class, because abstract class is "not concrete"
+        // We "can not create object" from Abstract class and interface, because abstract class is "not concrete"
 
 
-        Honda honda = new Honda("Accord", "Black", 2019, 30000);
+        Honda honda = new Honda("Honda","Civic","Blue",2019,30000);
 
-        Toyota audi = new Toyota("Corolla", "Comfort", "Blue", 2022,120000);
+        Toyota audi = new Toyota("Toyota", "Corolla", "Dark Blue", 2022,120000);
 
-        BMW tesla = new BMW("Model3", "White", 2021, 60000);
+        BMW tesla = new BMW("BMW", "Q5",   "White", 2020,100000);
+
+        CydeoCar cydeo=new CydeoCar("Cydeo","EU-10","Black",2023,130000);
 
         System.out.println(honda);
         System.out.println(audi);
         System.out.println(tesla);
+        System.out.println(cydeo);
 
         System.out.println("------------------------------------");
 
@@ -34,7 +37,12 @@ public class CarShop {
 
         System.out.println("------------------------------------");
 
-
+        honda.stop();
+        audi.start();
+        audi.autoPark();
+        tesla.selfDrive();
+        cydeo.fly();
+        System.out.println("Has cydeo auto park feature : "+CydeoCar.hasAutoPark);
 
     }
 
--- CoreJava/src/lectureTasks/day44_45_Interface/car/Honda.java
+++ CoreJava/src/lectureTasks/day44_45_Interface/car/Honda.java
@@ -3,15 +3,19 @@ package lectureTasks.day44_45_Interface.car;
 
 public final class Honda extends Car {
 
-    public Honda( String model, String color, int year, double price) {
-        super("Honda", model, color, year, price);
-    }
 
+    public Honda(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
 
     public void start(){
-        System.out.println("Twist the key to ignition");
+        System.out.println(getBrand()+" "+getModel()+" Twist the key to ignition");
     }
 
+    @Override
+    public void drive() {
+
+    }
 
 
 }
