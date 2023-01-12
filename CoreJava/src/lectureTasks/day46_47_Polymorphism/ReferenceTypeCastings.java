90bf3f24711c908867fc178d79008d31399e27de
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-12 10:57:22 +0300

    Abstraction/Inheritance/Interface projects have been uploaded.

--- CoreJava/src/lectureTasks/day46_47_Polymorphism/ReferenceTypeCastings.java
+++ CoreJava/src/lectureTasks/day46_47_Polymorphism/ReferenceTypeCastings.java
@@ -11,56 +11,57 @@ public class ReferenceTypeCastings {
     public static void main(String[] args) {
 
 
-     //   Shape shape = (Shape) new Circle(4); //upcasting
+        //   Shape shape = (Shape) new Circle(4); //upcasting
 
         Shape shape2 = new Circle(5);
 
         // WebDriver driver = (WebDriver) new ChromeDriver();
 
-       // Dog  dog2 = new Dog("Max", "Husky", 'M', 3, "Small", "White");;
-      //  Dog dog3 = dog2;
+        // Dog  dog2 = new Dog("Max", "Husky", 'M', 3, "Small", "White");;
+        //  Dog dog3 = dog2;
 
 
         Animal animal = new Dog("Max", "Husky", 'M', 3, "Small", "White");
 
-     //   Dog dog = (Dog)animal; // downcasting
-      //  dog.bark();
+        Dog dog = (Dog) animal; // downcasting
+        dog.bark();
 
-        System.out.println( animal.getName() );
-       // System.out.println( dog.getName() );
+        System.out.println(animal.getName());//We have only one dog object
+        System.out.println( dog.getName() );//We have only one dog object
 
-        (  (Dog)animal  ).bark();
+        // animal.bark(); Without casting we can not use dog methods from animal.
+        ((Dog) animal).bark();
 
 
         Shape shape1 = new Square(5);
 
-        System.out.println( ((Square) shape1).getSide()  );
+        System.out.println(((Square) shape1).getSide());
 
 
         System.out.println("---------------------------------------------------");
 
         Animal animal2 = new Cat("Jim", "Scotish", 'M', 3, "Small", "White");
 
-        //Cat cat = (Cat) animal2;
-       // cat.meow();
+        //Cat cat = (Cat) animal2; They are same object. We can use this more than one. We can all over and over again.
+        //animal2.meow(); We can not call this method from animal. Since it is only unique for object type.
 
-        ( (Cat) animal2 ).meow();
+        ((Cat) animal2).meow();
 
-        // ( (Dog) animal2 ).bark();
+        // ( (Dog) animal2 ).bark(); My object type is Cat. No inheritence so trows ClassCastException. IS A relationship is a must
 
         System.out.println("---------------------------------------------------");
 
-        Employee employee = new Tester("Ali",30,'M',42,"SDET",145000.00);
+        Employee employee = new Tester("Ali", 30, 'M', 42, "SDET", 145000.00);
 
-        ( (Tester)employee ).bugReport();
+        ((Tester) employee).bugReport();
 
-      //  ( (Tester)employee ).unitTest();
+        //  ( (Tester)employee ).unitTest();It is a checked error. Tester can not call developer method.
 
-      //  ( (Developer)employee ).unitTest();
+        //  ( (Developer)employee ).unitTest();My object type is tester. No inheritence so trows ClassCastException. IS A relationship is a must
 
-      //  Driver driver = (Driver) employee;  // line 1
+        //  Driver driver = (Driver) employee;  // line 1
         Person person = (Person) employee; // line 2
-    //    Teacher teacher = (Teacher) employee; // line 3
+        //    Teacher teacher = (Teacher) employee; // line 3
 
         System.out.println("----------------------------------------------------");
 
@@ -68,14 +69,7 @@ public class ReferenceTypeCastings {
         s1.area();
         s1.perimeter();
 
-      ( (Cube)s1 ).volume();
-
-
-
-
-
-
-
+        ((Cube) s1).volume();
 
 
     }
