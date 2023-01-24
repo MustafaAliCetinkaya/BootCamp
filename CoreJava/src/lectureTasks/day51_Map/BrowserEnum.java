6e8c54947ac437c5679a7021f1bfa1a1454ee4f1
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-24 16:40:00 +0300

    Collections projects have been uploaded

---
+++ CoreJava/src/lectureTasks/day51_Map/BrowserEnum.java
@@ -0,0 +1,11 @@
+package lectureTasks.day51_Map;
+
+public enum BrowserEnum {
+
+    CHROME,
+    FIREFOX,
+    OPERA,
+    EDGE,
+    SAFARI
+
+}
---
+++ CoreJava/src/lectureTasks/day51_Map/LevelEnum.java
@@ -0,0 +1,12 @@
+package lectureTasks.day51_Map;
+
+public enum LevelEnum {
+
+    EASY,
+    MEDIUM,
+    HARD,
+    ADVANCED,
+    EXPERT,
+    CHUCK_NORRIS
+
+}
