270e4183bae4e9a9f14ecadf3f5debbe3cb9a228
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-24 20:40:28 +0300

    Collections projects have been uploaded

--- CoreJava/src/lectureTasks/day51_Map/IteratingTheMap.java
+++ CoreJava/src/lectureTasks/day51_Map/IteratingTheMap.java
@@ -9,45 +9,46 @@ public class IteratingTheMap {
         Map<String, Integer> students = new HashMap<>();
         students.put("Aygun", 95);
         students.put("Maria", 90);
-        students.put("Ali",85);
-        students.put("Alex",80);
-        students.put("Ozan",99);
-        students.put("Serkan",70);
-        students.put("Andriy",98);
+        students.put("Ali", 85);
+        students.put("Alex", 80);
+        students.put("Ozan", 99);
+        students.put("Serkan", 70);
+        students.put("Andriy", 98);
 
-        /*
         Set<String> keys = students.keySet();
         for (String eachKey : keys) {
-         //   System.out.println(eachKey +" : " + students.get(eachKey));
+            //   System.out.println(eachKey +" : " + students.get(eachKey));
             students.replace(eachKey, students.get(eachKey) + 5);
         }
-*/
 
         System.out.println(students);
 
         Map<String, Integer> earlyBirds = new HashMap<>(); // score >= 90
         Map<String, Integer> angryBirds = new HashMap<>(); // score < 90
-
+        int countMoreThan90 = 0;
+        int countLessThan90 = 0;
         for (String key : students.keySet()) {
             Integer value = students.get(key);
-            if(value >= 90){
+            if (value >= 90) {
                 earlyBirds.put(key, value);
-            }else{
+                countMoreThan90++;
+            } else {
                 angryBirds.put(key, value);
+                countLessThan90++;
             }
         }
 
 
-        System.out.println("earlyBirds = " + earlyBirds);
-        System.out.println("angryBirds = " + angryBirds);
+        System.out.println("earlyBirds = " + earlyBirds + ", Number of early birds: " + countMoreThan90);
+        System.out.println("angryBirds = " + angryBirds + ", Number of angry birds: " + countLessThan90);
 
 
         Set<String> names = students.keySet();
 
         System.out.println("--------------------------------------------------------");
 
-       // Collection<Integer> scores = students.values();
-        List<Integer> scores = new ArrayList<>( students.values() );
+        // Collection<Integer> scores = students.values();
+        List<Integer> scores = new ArrayList<>(students.values());
 
         System.out.println(scores);
 
@@ -62,12 +63,12 @@ public class IteratingTheMap {
         int minScore = Integer.MAX_VALUE;
 
         for (Integer score : students.values()) {
-            if(score > maxScore){
+            if (score > maxScore) {
                 maxScore = score;
             }
 
-            if(score < minScore){
-                minScore =score;
+            if (score < minScore) {
+                minScore = score;
             }
 
         }
@@ -77,8 +78,8 @@ public class IteratingTheMap {
 
         System.out.println("--------------------------------------------------------");
 
-        int max = Collections.max( students.values() );
-        int min = Collections.min( students.values() );
+        int max = Collections.max(students.values());
+        int min = Collections.min(students.values());
 
         System.out.println("max = " + max);
         System.out.println("min = " + min);
@@ -88,37 +89,31 @@ public class IteratingTheMap {
         // how many students has the score of 95 or greater
 
         int count = 0;
+        int count1 = 0;
 
 
         for (Integer eachScore : students.values()) {
-            if(eachScore >= 95){
+            if (eachScore >= 95) {
                 count++;
             }
         }
 
-/*
         for (String eachKey : students.keySet()) {
             Integer eachScore = students.get(eachKey);
             if(eachScore >= 95){
                 count++;
             }
         }
-*/
         System.out.println(count);
+        System.out.println(count1);
 
 
         System.out.println("--------------------------------------------------------");
 
         for (Map.Entry<String, Integer> entry : students.entrySet()) {
             //System.out.println(entry);
-            System.out.println(entry.getKey() +" : " +entry.getValue());
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
-
-
-
-
-
-
 
 
     }
