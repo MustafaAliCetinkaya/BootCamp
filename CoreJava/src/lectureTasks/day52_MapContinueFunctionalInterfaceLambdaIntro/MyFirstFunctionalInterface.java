a839dd8d1652bf2bbdab40c4a69345f55d43bd6f
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-26 21:09:36 +0300

    Collections projects have been uploaded

--- CoreJava/src/lectureTasks/day52_MapContinueFunctionalInterfaceLambdaIntro/MyFirstFunctionalInterface.java
+++ CoreJava/src/lectureTasks/day52_MapContinueFunctionalInterfaceLambdaIntro/MyFirstFunctionalInterface.java
@@ -3,7 +3,7 @@ package lectureTasks.day52_MapContinueFunctionalInterfaceLambdaIntro;
 @FunctionalInterface
 public interface MyFirstFunctionalInterface {
 
-    void apply(int n);
+    public void apply(int n);
 
 
 }
