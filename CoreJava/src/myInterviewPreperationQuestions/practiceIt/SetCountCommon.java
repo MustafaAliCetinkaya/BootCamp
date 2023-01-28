a6fc09d1fa8dd1170020c2c9a117a093ac9673d2
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-01-28 21:20:45 +0300

    New projects have been uploaded

--- CoreJava/src/myInterviewPreperationQuestions/practiceIt/SetCountCommon.java
+++ CoreJava/src/myInterviewPreperationQuestions/practiceIt/SetCountCommon.java
@@ -11,15 +11,23 @@ list contains the values [-5, 15, 2, -1, 7, 15, 36], your method should return 4
 (because the elements -1, 2, 7, and 15 occur in both lists).*/
 public class SetCountCommon {
     public static void main(String[] args) {
-        List<Integer>list1=new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
         list1.addAll(Arrays.asList(3, 7, 3, -1, 2, 3, 7, 2, 15, 15));
-        List<Integer>list2=new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
         list2.addAll(Arrays.asList(-5, 15, 2, -1, 7, 15, 36));
 
         System.out.println("===================================================");
-
-
-
+        List<Integer> temp=new ArrayList<>();
+        for (Integer eachList1 : list1) {
+            for (int i = 0; i < list2.size(); i++) {
+                if (eachList1 == list2.get(i)) {
+                    if(!temp.contains(eachList1)){
+                        temp.add(eachList1);
+                    }
+                }
+            }
+        }
+        System.out.println(temp.size());
         System.out.println("===================================================");
 
         Set<Integer> set = new HashSet<Integer>(list1);
